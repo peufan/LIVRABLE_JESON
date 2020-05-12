db60561aad1c7fd0e6e7578ce355139e66a22382
SAWADOGO <sawadogoab95@gmail.com>
2020-05-12 12:18:43 +0000

    fichier ok

---
+++ F.jeson.js
@@ -0,0 +1,76 @@
+{ 
+    "Name";"Schema",
+    "field";[
+	{
+        "name":"HDI_rank",
+        "type":"integer",
+        "description":"Indice de Developpement Humain."
+    },
+    {
+        "name":"HDI",
+        "type":"string",
+        "description":"Indice de developpement humain."
+	},
+	{
+        "name":"Country",
+        "type":"string",
+        "description":"nom de l'un des 177 pays ici mentionné."
+	},
+	{
+        "name":"HIV_prevalencea(percentage_aged_15-49)2006_range_estimate",
+        "type":"array",
+        "description":"estimation de la prevalance HIV par intervalle."
+        },
+	{
+        "name":"HIV_prevalencea(percentage_aged_15-49)2006_point_estimate",
+        "type":"number",
+        "description":"estimation ponctuelle HIV prevalence."
+	},
+	{
+        "name":"Condom_use_at_last_high-risk_sexb(percentage_aged_15-24) _Women1999-2005g",
+        "type":"integer",
+        "description":"utilisation de preservatif chez les jeunes femme de 15 à 24 ans."
+	},
+	{
+        "name":"Condom_use_at_last_high-risk_sexb(percentage_aged15-24)Men1999-2005g",
+        "type":"integer",
+        "decription":"utilisation de preservatif chez les jeunes homme de 15 à 24 ans."
+	},
+	{
+        "name":"Antimalarial_measures_Use_of_insecticide—treated(percentage_of_children_under_five)_1999-2005_bednets",
+        "type":"integer",
+        "description":"le pourcentage d'enfants moins de 5 ans ayant beneficié mousiquaires dans la lutte contre le paludisme."
+	},
+	{
+        "name":"Antimalarial_measures_Fevers_treated with(percentage_of_children_under_five)1999-2005_antimalarial_drugs",
+        "type":"integer",
+        "description":"poucentage d'enfants moins de 5 ans ayant beneficié d'un traitement de fievre dans la lutte contre le paludisme."
+	},
+	{
+        "name":"Tuberculosis_cases_Prevalence2005",
+        "type":"integer",
+        "description":"prevalence en TB."
+	},
+	{
+        "name":"Tuberculosis_cases_Detected_under_DOTSd(percentage)2005",
+        "type":"integer",
+        "description":"TB detecté sur persones sous DOTS."
+	},
+	{
+        "name":"Tuberculosis_cases Cured_under_DOTSe(percentage)2004",
+        "type":"integer",
+        "description":"les cas de TB guéris sous DOTS (%)."
+	},
+	{
+        "name":"Prevalence_of_ smoking_Women(percentage_of_adults)f2002-04g",
+        "type":"integer",
+        "description":"prevalence du tabagisme chez les  femmes  adultes (%)."
+    },
+    {
+        "name":"Prevalenc_ of_smoking_Men(percentage_of_adults)f2002-04g",
+        "type":"integer",
+        "description":"prevalence du tabagisme chez les  hommes  adultes (%)"
+    }
+	],
+    "missingValues"; ["NaN", "..", " "]
+}
